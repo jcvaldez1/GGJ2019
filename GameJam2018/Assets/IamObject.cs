daf1f490239be5a859671ee32e9ad0898fdaffcc
jcvaldez1 <jcvaldez1@up.edu.ph>
2019-01-26 22:04:35 +0800

    added some colliders

--- GameJam2018/Assets/IamObject.cs
+++ GameJam2018/Assets/IamObject.cs
@@ -6,13 +6,41 @@ public class IamObject : MonoBehaviour{
 
 	public bool used; // FOR WHEN THE OBJECT IS ALREADY USED
 	public bool reusable;
-	public bool firstUsed;
-	public Interactable actionDefns;
+	public GameObject Alternate;
+	public GameObject current;
+	public int tasknumber;
+
+	void Start() {
+	
+	}
+
 	public void Use() {
-		if (reusable || !firstUsed) {
+		if (reusable || !used) {
 			used = !used;
-			firstUsed = false;
-			actionDefns.isUsed ();
+			Vector3 current_position =  transform.position;
+			// spawn Alternate prefab here
+
+			// destroy current
+			Destroy(this.gameObject);
+
+			GameObject temp = current;
+			current = Alternate;
+			Alternate = temp;
+
+
+			Debug.Log ("HELLO");
+			GameObject newboi = GameObject.Instantiate (current);
+			newboi.transform.position = current_position;
+			// render current,  in curren_position
+
+			// open checklist that this object has been done
+			
+		}
+	}
+
+	public void requirementCheck(bool check) {
+		if (check) {
+			Use ();
 		}
 	}
 }
